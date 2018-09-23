7dae732d637e35d1ba9d2c3036a1b05cac8609cb
yogialfin23 <43507570+yogialfin23@users.noreply.github.com>
2018-09-23 18:39:51 +0700

    Add files via upload

---
+++ PBO3_10117100_Latihan2_Deklarasi_Pengaksesan_Variabel.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3_10117100_latihan2_deklarasi_pengaksesan_variabel;
+
+/**
+ * Yogie Alfin Salim
+ * 10117100
+ * IF-3
+ */
+public class PBO3_10117100_Latihan2_Deklarasi_Pengaksesan_Variabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Deklarasi Variabel
+       int nilaiInt;
+       final double PHI = 3.14;
+       boolean nilaiLogika;
+       char nilaiKarakter;
+       
+       // Memberi Nilai ke Variabel
+       nilaiInt = 78;
+       nilaiLogika = false;
+       nilaiKarakter = 'D';
+       
+       //Menampilkan Hasil
+        System.out.println();
+        System.out.println("Isi Variabel Nilai = " + nilaiInt);
+        System.out.println("Isi Variabel PHI = " +PHI);
+        System.out.println("Isi Variabel Logika = " + nilaiLogika);
+        System.out.println("Isi Variabel Karakter = " + nilaiKarakter);
+       
+       
+    }
+    
+}
